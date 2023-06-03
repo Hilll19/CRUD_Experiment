881223f90e6b381a81be9772137da0fcb9f8faac
Hilll19 <hilworking0110@gmail.com>
2023-06-03 15:19:23 +0700

    create requets.rest, update controller and router

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -8,3 +8,18 @@ export const getUsers = async(req, res) =>{
         console.log(error.message);
     }
 }
+
+//To get single data 
+export const getUserById = async(req, res) =>{
+    try {
+        const response = await User.findOne({
+            //To get id from params
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,10 +1,13 @@
 //This file ti handle all routing file
 import express from "express";
-import {getUsers} from "../controller/UserController.js";
+import {
+    getUsers, 
+    getUserById
+} from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get('/Users', getUsers); //New endpoint
-
+router.get('/Users/:id', getUserById); //To get id add parameter :id
 
 export default router;
