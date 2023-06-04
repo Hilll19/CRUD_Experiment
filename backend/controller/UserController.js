9856b461e2d2a922110f935bbafce9e3e91441b8
Hilll19 <hilworking0110@gmail.com>
2023-06-04 21:23:54 +0700

    add code for user create, update, and delete

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -23,3 +23,43 @@ export const getUserById = async(req, res) =>{
         console.log(error.message);
     }
 }
+
+//For createUser
+export const createUser = async(req, res) =>{
+    try {
+        await User.create(req.body);
+        res.status(201).json({msg: "User Created"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//For update user, metode use id req params
+export const updateUser = async(req, res) =>{
+    try {
+        const response = await User.update(req.body,{
+            //To get id from params
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "User updated"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//For delete User
+export const deleteUser = async(req, res) =>{
+    try {
+        const response = await User.destroy({
+            //To get id from params
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "User deleted"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -2,12 +2,19 @@
 import express from "express";
 import {
     getUsers, 
-    getUserById
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 } from "../controller/UserController.js";
 
 const router = express.Router();
 
 router.get('/Users', getUsers); //New endpoint
 router.get('/Users/:id', getUserById); //To get id add parameter :id
+router.post('/Users/', createUser); //This router to created user
+router.patch('/Users/:id', updateUser); //This update user by user id with router patch
+router.delete('/Users/:id', deleteUser); //This to deleted user 
+
 
 export default router;
