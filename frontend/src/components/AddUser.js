e0f3246c3ab31c69d3ad27ac7167598e95251237
Hilll19 <hilworking0110@gmail.com>
2023-06-08 15:06:57 +0700

    Add user update likeusernavigate userstate

--- frontend/src/components/AddUser.js
+++ frontend/src/components/AddUser.js
@@ -1,40 +1,83 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
-  return (
-    <div className='columns mt-5 is-centered'>
-        <div className='column is-half'>
-            <form action="">
-                <div className='field'>
-                    <label className="label">Name</label>
-                    <div className="control">
-                        <input type="text" className="input" placeholder='Name'/>
-                    </div>
-                </div>
-                <div className='field'>
-                    <label className="label">Email</label>
-                    <div className="control">
-                        <input type="text" className="input" placeholder='Email'/>
-                    </div>
-                </div>
-                <div className='field'>
-                    <label className="label">Gender</label>
-                    <div className="control">
-                        <div className="select is-fullwidth">
-                            <select>
-                                <option value="Male">Male</option>
-                                <option value="Female">Female</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div className='field'>
-                    <button type='submit' className='button is-succes'>Save</button>
-                </div>
-            </form>
-        </div>
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const navigate = useNavigate();
 
-export default AddUser
+  const saveUser = async (e) => {
+    //Add preventDefault when submit, page not reload
+    e.preventDefault();
+    try {
+        await axios.post('http://localhost:5000/users', {
+        //For data post, data use from state
+        name,
+        email,
+        gender
+        }); //If succes submited and save will redirect to home.Use useNavigate from react router dom
+        navigate("/"); //for "/" its mean home
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <form onSubmit={saveUser}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                //This function to update state
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={email}
+                //This function to update state
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Gender</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={gender}
+                  //This function to update state
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <button type="submit" className="button is-succes">
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
